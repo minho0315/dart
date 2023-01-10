31a0c440bc2348a2de470527919d5035f12ba6ab
minho0315 <dlalsgh369@naver.com>
2023-01-10 16:28:00 +0900

    Data types

--- main.dart
+++ main.dart
@@ -1,6 +1,44 @@
 void main() {
-  late final String name;
-  const String name2 = 'min';
-  name = 'minho';
-  print(name);
+  // String name = "nice";
+  // bool alive = true;
+  // int age = 12;
+  // double mony = 69.99;
+  // print(name);
+
+  // var numbers = [
+  //   1,
+  //   2,
+  //   3,
+  //   4,
+  //   if (true) 5,
+  // ];
+
+  // print(numbers);
+
+  // var name = 'minho';
+  // var age = 10;
+  // var greeting = 'Hello everyone, my name is $name and I\'m ${age + 2}';
+  // print(greeting);
+
+  // var oldFriends = ['minho', 'min'];
+  // var newFrineds = [
+  //   'ho',
+  //   'hu',
+  //   for (var friend in oldFriends) "❤ $friend",
+  // ];
+  // print(newFrineds);
+
+  // Map<String, Object> player = {
+  //   'name': 'minho',
+  //   'xp': 1123,
+  // };
+
+  // print(player);
+
+  var numbers = {1, 2, 3, 4};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
 }
